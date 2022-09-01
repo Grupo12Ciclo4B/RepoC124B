f036338b399c23ded12458fdcf6712951a22c4e9
Dioclexvian <dfsuarezs.ing@gmail.com>
2022-08-31 19:45:26 -0500

    animacion y creacion del proyecto

---
+++ app/src/main/java/com/example/apprende4bg12/SplashActivity.kt
@@ -0,0 +1,72 @@
+package com.example.apprende4bg12
+
+import android.animation.Animator
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.apprende4bg12.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.splashAnimation.playAnimation()
+
+        binding.splashAnimation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Log.d("hola","animacion finalizada")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+
+        })
+
+        binding.splash2Animation.playAnimation()
+        binding.splash2Animation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Log.d("golas","finalizado animacion 2")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+        } )
+    }
+}
