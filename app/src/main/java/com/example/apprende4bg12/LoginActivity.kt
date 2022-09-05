3203ed979b2cadda9d3227bef0ba8ba56968bf27
Carlostal <carlosandrestalerom@gmail.com>
2022-09-04 22:23:10 -0500

    Vinculacion de las actividades de log in, sign up y reset password.

--- app/src/main/java/com/example/apprende4bg12/LoginActivity.kt
+++ app/src/main/java/com/example/apprende4bg12/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.apprende4bg12
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.apprende4bg12.databinding.ActivityLoginBinding
 
 
@@ -12,5 +14,20 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val password = findViewById<Button>(R.id.pass)
+
+        password.setOnClickListener{
+            val intent = Intent(this, PasswordReset::class.java)
+            startActivity(intent)
+        }
+
+        val signup = findViewById<Button>(R.id.signupbtn)
+
+        signup.setOnClickListener{
+            val intent2 = Intent(this, SignupActivity::class.java)
+            startActivity(intent2)
+        }
+
     }
 }
