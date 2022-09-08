b10cf3bbf476d0400a4b1e7b34a9965f76491884
Dioclexvian <dfsuarezs.ing@gmail.com>
2022-09-08 08:41:06 -0500

    activities como fragments mas pantalla home

--- app/src/main/java/com/example/apprende4bg12/LoginFragment.kt
+++ app/src/main/java/com/example/apprende4bg12/LoginFragment.kt
@@ -1,10 +1,12 @@
 package com.example.apprende4bg12
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.apprende4bg12.databinding.FragmentLoginBinding
 
 
@@ -22,5 +24,37 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
 
+        binding.loginbtn.setOnClickListener{
+            if (!binding.loginEmail.text.toString().isValidEmail()){
+                binding.loginEmailLayout.error = getString(R.string.Email_error)
+            }else{
+                binding.loginEmailLayout.error = null
+            }
+
+            if (!binding.loginPassword.text.toString().isValidPassword()){
+                binding.loginPasswordLayout.error = getString(R.string.Password_error)
+            }else{
+                binding.loginPasswordLayout.error = null
+            }
+
+            if(binding.loginEmail.text.toString().isValidEmail() && binding.loginPassword.text.toString().isValidPassword()){
+                val intent = Intent(requireContext(), HomeActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            }
+        }
+
+        binding.signupbtn.setOnClickListener{
+
+            findNavController().navigate(R.id.LoginToSignup)
+
+        }
+
+        binding.ForgotPassword.setOnClickListener{
+            findNavController().navigate(R.id.LoginToRestarPassword)
+        }
+    }
 }
