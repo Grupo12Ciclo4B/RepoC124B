911ea9f69336aa6a00e7cc7e429795e3ab84de24
Dioclexvian <dfsuarezs.ing@gmail.com>
2022-09-12 17:28:09 -0500

    se crea metodo dinamico para llamar categorias y se ingresa imagenes de categorias y cursos, finanzas y negocios comparten todo excepto admin y negociosint, falta replicar lo mismo para los cursos, el evento onclick ya lo lee, se verifica en consola

---
+++ app/src/main/java/com/example/apprende4bg12/OnServiceClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.apprende4bg12
+
+interface OnServiceClickListener {
+    fun onClick (item: ServiceModel)
+}
---
+++ app/src/main/java/com/example/apprende4bg12/ServiceModel.kt
@@ -0,0 +1,12 @@
+package com.example.apprende4bg12
+
+import java.io.FileDescriptor
+
+data class ServiceModel (
+    val id: String,
+    val icon: String,
+    val title: String,
+    val description: String,
+    )
+
+
